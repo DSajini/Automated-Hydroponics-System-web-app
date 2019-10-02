2fb2d49fafec3210af8da351b2ec685a646998f7
Sajini Ishanthi <dsajini.i@gmail.com>
2019-10-02 16:03:46 +0530

    Update firebase.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,25 +2,15 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-/*
-//for prototype
-const config = {
-    apiKey: "AIzaSyDTVcpBm_d9g8pALjt7CKTkX3n3Byp0qjA",
-    authDomain: "neww-3600d.firebaseapp.com",
-    databaseURL: "https://neww-3600d.firebaseio.com",
-    projectId: "neww-3600d",
-    storageBucket: "neww-3600d.appspot.com",
-    messagingSenderId: "782359771130",
-};
-*/
+
 
 const config = {
-  apiKey: "AIzaSyCw0adhmMWW1DYDG530pD7kL3KO3GaCRnA",
-  authDomain: "test1-64187.firebaseapp.com",
-  databaseURL: "https://test1-64187.firebaseio.com",
-  projectId: "test1-64187",
-  storageBucket: "test1-64187.appspot.com",
-  messagingSenderId: "570880262877",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
   
 };
 
